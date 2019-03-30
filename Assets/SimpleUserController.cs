72fa1d2e239b0718cf10ec3ce26bbc12dfb8d868
Miguel Campos <amodestduck@gmail.com>
2019-03-30 16:09:27 -0700

    Adding Final Dropdown Menu
    Feature.
    -
    -

--- Assets/SimpleUserController.cs
+++ Assets/SimpleUserController.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SimpleUserController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject[] models;
+    [SerializeField] Dropdown dropdown;
+    private void Awake()
     {
-        
+        dropdown.onValueChanged.AddListener(SetModel);
+        Debug.Log("Called####");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetModel(int index)
     {
-        
+        for(int i = 0; i < models.Length; i++)
+        {
+            Debug.Log($"Called: {index}");
+            if (i == index)
+            {
+                models[i].SetActive(true);
+
+            }
+            else
+            {
+                models[i].SetActive(false);
+            }
+        }
     }
 }
